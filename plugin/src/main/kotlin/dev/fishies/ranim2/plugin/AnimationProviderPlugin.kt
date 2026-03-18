443f906122f0ad84aa7068f63335798f6f139faf
fish <me@fishies.dev>
2026-03-18 03:07:57 -0700

    Apparently all I needed was a thread

--- plugin/src/main/kotlin/dev/fishies/ranim2/plugin/AnimationProviderPlugin.kt
+++ plugin/src/main/kotlin/dev/fishies/ranim2/plugin/AnimationProviderPlugin.kt
@@ -10,6 +10,7 @@ import org.gradle.kotlin.dsl.*
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.ProjectConnection
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmExtension
+import kotlin.concurrent.thread
 
 const val composeVersion = "1.10.0"
 const val kotlinVersion = "2.3.0"
@@ -45,7 +46,6 @@ private fun Project.applyAnimationProviderPlugin() {
     }
 
     dependencies {
-        add("implementation", "org.jetbrains.compose.components:components-resources:$composeVersion")
         add("implementation", "dev.fishies.ranim2:core:1.0.0")
         add("guiConfig", "dev.fishies.ranim2:gui:1.0.0")
         add("guiConfig", "dev.fishies.ranim2:core:1.0.0")
@@ -58,40 +58,32 @@ private fun Project.applyAnimationProviderPlugin() {
 
     val watchSources = tasks.register("watchSources") {
         doLast {
-            GradleConnector.newConnector()
-                .forProjectDirectory(project.projectDir)
-                .connect()
-                .use { connection: ProjectConnection ->
-                    connection.newBuild()
-                        .forTasks("jar")
-                        .addArguments("--continuous")
-                        .setStandardInput(System.`in`)
-                        .setStandardOutput(System.out)
-                        .run()
-                }
+            thread(start = true) {
+                GradleConnector.newConnector()
+                    .forProjectDirectory(project.projectDir)
+                    .connect()
+                    .use { connection: ProjectConnection ->
+                        connection.newBuild()
+                            .forTasks("jar")
+                            .addArguments("--continuous")
+                            .setStandardInput(System.`in`)
+                            .setStandardOutput(System.out)
+                            .run()
+                    }
+            }
         }
     }
 
-    // val runGui = tasks.register<JavaExec>("runGui") {
-    //     dependsOn(tasks.named("jar"))
-    //     inputs.file(jsonFile)
-    //     // println(ManagementFactory.getRuntimeMXBean().inputArguments)
-    //     debug = true
-    //     args.addAll(ManagementFactory.getRuntimeMXBean().inputArguments)
-    //     classpath = guiConfig
-    //     mainClass = "dev.fishies.ranim2.gui.MainKt"
-    //     args = listOf(jsonFile.absolutePath)
-    // }
     val runGui = tasks.register<JavaExec>("runGui") {
         dependsOn(tasks.named("jar"))
         inputs.file(jsonFile)
-        debug = true
+        // debug = true
         classpath = guiConfig
         mainClass = "dev.fishies.ranim2.gui.MainKt"
         args = listOf(jsonFile.absolutePath)
     }
 
     tasks.register("gui") {
-        dependsOn(runGui)
+        dependsOn(watchSources, runGui)
     }
 }
--- test/src/main/kotlin/ColorPickerAnimation.kt
+++ test/src/main/kotlin/ColorPickerAnimation.kt
@@ -92,3 +92,6 @@ fun colorPickerAnimation() = animation {
         )
     }
 }
+
+@AnimationProvider
+fun anotherAnimation() = animation {  }
